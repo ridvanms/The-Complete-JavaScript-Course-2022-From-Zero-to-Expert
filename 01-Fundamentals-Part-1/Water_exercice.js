8d758842f6c450c4c455ffb8e5e38ee8248eaa0f
Ridvan <103427078+ridvanms@users.noreply.github.com>
2022-04-17 23:32:27 +0300

    Create Water_exercice.js

---
+++ 01-Fundamentals-Part-1/Water_exercice.js
@@ -0,0 +1,7 @@
+const age = 23;
+age >= 18 ? console.log("I like to drink wine 🍷"):
+console.log("i like to drink water");
+
+const drink = age  >= 18 ? "bira 🥃": "cappy 🍸";
+
+console.log(`I like to drink ${age >= 23 ? " bira": " water"}`);
