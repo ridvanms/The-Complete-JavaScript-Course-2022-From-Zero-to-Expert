f1ee8fdb15f4c4ac6d8981bdceec67fe83232492
Ridvan <ridvans@abv.bg>
2023-01-19 23:47:19 +0200

    updating

--- forkify/src/js/config.js
+++ forkify/src/js/config.js
@@ -2,7 +2,7 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '10712d56-3c66-4c6c-bd5f-45e3615d08f1';
-export const MODAL_CLOSE_SEC = 2.5;
+export const MODAL_CLOSE_SEC = 2.5; st
 /*
 # you could get the KEY on:
 
